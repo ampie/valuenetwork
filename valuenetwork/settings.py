4106785bf1f7740e86efde83e54acf2d0a4355d9
ampie <ampieb@gmail.com>
2013-08-11 17:27:53 +0200

    First working version in Jython/Wildfly

--- valuenetwork/settings.py
+++ valuenetwork/settings.py
@@ -15,10 +15,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
-        "ENGINE": "doj.backends.zxjdbc.postgresql",
+        "ENGINE": "doj.backends.zxjdbc.mysql",
         "NAME": "valuedb",
-        "USER": "postgres",
-        "PASSWORD" : "postgres"
+        "USER": "root",
+        "PASSWORD" : "ainnikki"
     }
 }
 
@@ -65,15 +65,17 @@ MEDIA_URL = "/site_media/media"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
+STATIC_ROOT = os.path.join(PACKAGE_ROOT, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/site_media/static"
+STATIC_URL = "/static/valuenetwork/"
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(PACKAGE_ROOT, "static"),
+    os.path.join(PACKAGE_ROOT, "static", "valuenetwork"),
+    os.path.join(PACKAGE_ROOT, "static", "bootstrap"),
+    os.path.join(PACKAGE_ROOT, "static", "pinax"),
 ]
 
 # List of finder classes that know how to find static files in
@@ -135,12 +137,15 @@ INSTALLED_APPS = [
     'django.contrib.databrowse',
     
     # theme
+    "pinax_utils",
     "pinax_theme_bootstrap_account",
     "pinax_theme_bootstrap",
     "django_forms_bootstrap",
     
     # external
     "account",
+    "pytz",
+    "appconf",
     "metron",
     #'debug_toolbar',
     'django_extensions',
